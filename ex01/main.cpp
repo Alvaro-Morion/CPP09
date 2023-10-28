c0555410e445ece181145a2a5477084186a2ca29
Alvaro Morion de eusebio <amorion-@c2r4s4.42madrid.com>
2023-10-28 18:04:48 +0200

    ex00ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amorion- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/28 17:20:25 by amorion-          #+#    #+#             */
+/*   Updated: 2023/10/28 18:04:27 by amorion-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include"RPN.hpp"
 #include<cctype>
 #include<stdlib.h>
@@ -7,62 +19,60 @@ int main(int argc, char **argv)
 	if(argc != 2)
 	{
 		std::cerr << "Error\n";
-		return(1);
+		return(-1);
 	}
-	std::string line(argv[0]);
+	std::string line(argv[1]);
 	RPN rpn;
 	for (std::string::iterator it = line.begin(); it != line.end(); it++)
 	{
 		std::string substr;
 		if(*it == ' ')
-		{}
+			continue;
 		else if(isdigit(*it))
 		{
-			while(isdigit(*it))
-			{
-				substr += *it;
-				it++;
-			}
-			if(*it != ' ')
+			if(it + 1 != line.end() && *(it + 1) != ' ')
 			{
 				std::cerr << "Error\n";
 				return(1);
 			}
+			substr += *it;
 			try
 			{
-				rpn.InsertNumber(atoi(substr));
+				rpn.InsertNumber(atoi((substr.c_str())));
 			}
-			catch std::exception &e
+			catch (std::exception &e)
 			{
 				std::cerr << "Error\n";
-				return(1);
+				return(2);
 			}
 		}
 		else
 		{
-			while(*it != ' ')
-			{
-				substr += *it;
-				it++;
-			}
-			try
-			{
-				rpn.operate(substr);
-			}
-			catch std::exception &e
+			if (it + 1 != line.end() && *(it + 1) != ' ')
 			{
 				std::cerr << "Error\n";
-				return(1);
+				return(3);
+			}
+			substr += *it;
+			try
+			{
+				rpn.Operate(substr);
+			}
+			catch (std::exception &e)
+			{
+				std::cerr << "Error\n";
+				return(4);
 			}
 		}
+	}
 	try
 	{
 		rpn.DisplayResult();
 	}
-	catch std::exception &e
+	catch (std::exception &e)
 	{	
 		std::cerr << "Error\n";
-		return(1);
+		return(5);
 	}
-	return(0)
+	return(0);
 }
